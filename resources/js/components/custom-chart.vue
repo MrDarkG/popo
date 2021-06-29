2675089b9602dc8c4b7e6983e8ee9d5e55169d75
MrDarkG <vako.batiashvili1998@gmail.com>
2021-06-30 01:20:35 +0400

    aa

--- resources/js/components/custom-chart.vue
+++ resources/js/components/custom-chart.vue
@@ -3,16 +3,17 @@
 	<VueSvgGauge
 	  :start-angle="-110"
 	  :end-angle="110"
-	  :value="3"
-	  :separator-step="1"
+	  :value="value"
+	  :separator-step="20"
 	  :min="0"
-	  :max="4"
-	  :gauge-color="[{ offset: 0, color: '#347AB0'}, { offset: 100, color: '#8CDFAD'}]"
-	  :scale-interval="0.1"
+	  :max="100"
+	  :gauge-color="[{ offset: 0, color: '#5710B2'}, { offset: 100, color: 'red'}]"
+	  :scale-interval="10"
+	  :inner-radius="radius"
 	>
-		<div class="inner-text">
-	    	<span>Let's make this <b>fun</b> !</span>
-	  	</div>
+		<div class="inner-text d-flex align-items-end justify-content-center">
+    	<span>თქვენი ქულა არის<b>{{ value }}</b> !</span>
+  	</div>
 	</VueSvgGauge>
 </div>
 </template>
@@ -21,16 +22,22 @@ import VueSvgGauge from 'vue-svg-gauge'
 Vue.use(VueSvgGauge)
 
 export default{
-
+	data(){
+		return {
+			radius:80,
+			value:60,
+		}
+	}
 };
 </script>
 <style type="sass">
 .inner-text {
   height: 100%;
   width: 100%;
+  text-align: center;
 }
 .inner-text span {
     max-width: 100px;
-    color: red;
+    color: #5710b2;
   }
 </style>
